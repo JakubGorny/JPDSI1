e5f39910afb6934fd57b388f7caa65f3b9467bd4
JakubGorny <gornykuba272@gmail.com>
2022-03-22 09:14:36 +0100

    .

--- zad2/app/security/login_view.php
+++ zad2/app/security/login_view.php
@@ -1,14 +1,10 @@
 <!DOCTYPE HTML>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<html lang="pl">
 <head>
 	<meta charset="utf-8" />
 	<title>Logowanie</title>
-	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 </head>
 <body>
-
-<div style="width:90%; margin: 2em auto;">
-
 <form action="<?php print(_APP_ROOT); ?>/app/security/login.php" method="post" class="pure-form pure-form-stacked">
 	<legend>Logowanie</legend>
 		<label for="id_login">login: </label>
@@ -20,7 +16,6 @@
 </form>	
 
 <?php
-//wyświeltenie listy błędów, jeśli istnieją
 if (isset($messages)) {
 	if (count ( $messages ) > 0) {
 		echo '<ol style="padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
