f4e15ab76c7c910be6b1806a80af4dea36e63c39
JakubGorny <gornykuba272@gmail.com>
2022-04-10 22:04:34 +0200

    dodano szablonowanie smarty + template

---
+++ zad3_4 - smarty/app/calc.php
@@ -0,0 +1,84 @@
+
+<?php
+require_once dirname(__FILE__).'/../config.php';
+require_once _ROOT_PATH.'/lib/smarty/Smarty.class.php';
+
+
+//pobranie parametrów
+function getParams(&$form){
+	
+	$form['waga'] = isset($_REQUEST['waga']) ? $_REQUEST['waga'] : null;
+	$form['wzrost'] = isset($_REQUEST['wzrost']) ? $_REQUEST['wzrost'] : null;
+}
+
+//walidacja parametrów z przygotowaniem zmiennych dla widoku
+function validate(&$form,&$infos,&$msgs,&$hide_intro){
+
+	//sprawdzenie, czy parametry zostały przekazane - jeśli nie to zakończ walidację
+	if ( ! (isset($form['waga']) && isset($form['wzrost']) ))	return false;	
+	
+	//parametry przekazane zatem
+	//nie pokazuj wstępu strony gdy tryb obliczeń (aby nie trzeba było przesuwać)
+	// - ta zmienna zostanie użyta w widoku aby nie wyświetlać całego bloku itro z tłem 
+	$hide_intro = true;
+
+	$infos [] = 'Przekazano parametry.';
+
+	// sprawdzenie, czy potrzebne wartości zostały przekazane
+	if ( $form['waga'] == "") $msgs [] = 'Nie podano liczby 1';
+	if ( $form['wzrost'] == "") $msgs [] = 'Nie podano liczby 2';
+	
+	//nie ma sensu walidować dalej gdy brak parametrów
+	if ( count($msgs)==0 ) {
+		// sprawdzenie, czy $x i $y są liczbami całkowitymi
+		if (! is_numeric( $form['waga'] )) $msgs [] = 'Pierwsza wartość nie jest liczbą';
+		if (! is_numeric( $form['wzrost'] )) $msgs [] = 'Druga wartość nie jest liczbą';
+	}
+	
+	if (count($msgs)>0) return false;
+	else return true;
+}
+
+function process(&$form,&$infos,&$messages,&$result){
+	global $role;
+	$infos [] = 'Parametry poprawne. Wykonuję obliczenia.';
+	//konwersja parametrów na int
+	$form['waga'] = floatval($form['waga']);
+	$form['wzrost'] = floatval($form['wzrost']);
+	
+	//wykonanie operacji
+	$result = $form['waga'] / pow(($form['wzrost']/100),2);
+}
+
+//definicja zmiennych kontrolera
+$form = null;
+$infos = array();
+$messages = array();
+$result = null;
+$hide_intro = false;
+	
+
+//pobierz parametry i wykonaj zadanie jeśli wszystko w porządku
+getParams($form);
+if ( validate($form,$infos,$messages,$hide_intro) ){
+	process($form,$infos,$messages,$result);
+}
+
+$smarty = new Smarty();
+
+$smarty->assign('app_url',_APP_URL);
+$smarty->assign('root_path',_ROOT_PATH);
+$smarty->assign('page_title','Kalkulator BMI');
+$smarty->assign('page_description','Szablonowanie oparte na bibliotece Smarty');
+$smarty->assign('page_header','Szablony Smarty');
+
+$smarty->assign('hide_intro',$hide_intro);
+
+//pozostałe zmienne niekoniecznie muszą istnieć, dlatego sprawdzamy aby nie otrzymać ostrzeżenia
+$smarty->assign('form',$form);
+$smarty->assign('result',$result);
+$smarty->assign('messages',$messages);
+$smarty->assign('infos',$infos);
+
+// 5. Wywołanie szablonu
+$smarty->display(_ROOT_PATH.'/app/calc.html');
