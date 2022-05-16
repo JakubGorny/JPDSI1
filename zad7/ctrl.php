5c29b79ef280573b1ea4781d244d2fec91e38417
JakubGorny <gornykuba272@gmail.com>
2022-05-16 22:32:05 +0200

    routing

--- zad7/ctrl.php
+++ zad7/ctrl.php
@@ -12,7 +12,7 @@ require_once 'init.php';
 getRouter()->setDefaultRoute('calcShow'); // akcja/ścieżka domyślna
 getRouter()->setLoginRoute('login'); // akcja/ścieżka na potrzeby logowania (przekierowanie, gdy nie ma dostępu)
 
-
+getRouter()->addRoute('calcShow',    'CalcCtrl',  ['user','admin']);
 getRouter()->addRoute('calculate', 'CalcCtrl',  ['user','admin']);
 getRouter()->addRoute('login',       'LoginCtrl');
 getRouter()->addRoute('logout',      'LoginCtrl', ['user','admin']);
